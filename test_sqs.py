63e80978828472d3924abc4b07268fdcdcbdb922
jessica fan <jcf2167@columbia.edu>
2015-04-16 11:37:04 -0400

    working sqs w attr

--- test_sqs.py
+++ test_sqs.py
@@ -4,40 +4,35 @@ conn = boto.sqs.connect_to_region("us-west-2",aws_access_key_id='AKIAJLHKOR7Z5KB
 q = conn.create_queue('myqueue')
 
 m = Message()
+
 m.set_body("hi")
-m = Message()
 m.message_attributes = {
-	"name1": {
-		"data_type": "String",
-		"string_value": "I am a string"
-	},
-	"name2": {
-		"data_type": "Number",
-		"string_value": "12"
-	}
+    "name1": {
+        "data_type": "String",
+        "string_value": "I am a string"
+    },
+    "name2": {
+        "data_type": "Number",
+        "string_value": "12"
+    }
 }
 
 
 
-m1 = Message()
-m.set_body("hi")
-m1.set_body("hi1")
+
 print "these are all queues: " 
 print conn.get_all_queues()
 print "writing to queue"
 q.write(m)
-q.write(m1)
+
 print "getting queue messgae"
-rs = q.get_messages(2)
-rs_attr = q.get_messages(message_attributes=['name1', 'name2'])
+
+rs = q.get_messages(message_attributes=['name1', 'name2'])
 ma= rs[0].message_attributes['name1']['data_type']
+print "len(rs_attrs)"
 print len(rs)
+
+print "----"
+print rs[0].get_body()
+print rs[0].message_attributes['name1']['string_value']
 print "these are all messages:"
-print q.get_messages(2)
-m = rs[0]
-m1 = rs[1]
-print m.get_body()
-print m1.get_body()
-print q
-print "these are all messages:"
-print q.get_messages()
