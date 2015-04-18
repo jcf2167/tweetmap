576281afaf83796f71e8c5f9198d8a40ff7eff4a
jessica fan <jcf2167@columbia.edu>
2015-04-18 04:03:16 -0400

    sns http endpoint confirmation

--- application.py
+++ application.py
@@ -6,9 +6,10 @@ import threading
 import time 
 import mysql.connector
 import threading 
+import boto.sns
+from requests import get as rget
+
 from alchemyapi import AlchemyAPI
-
-
 import boto.sqs
 from boto.sqs.message import Message
 
@@ -20,9 +21,8 @@ DATABASE = 'coord.db'
 app = Flask(__name__)
 alchemyapi = AlchemyAPI()
 
-
 fake_db=[]
-# Authentication details. To  obtain these visit dev.twitter.com
+
 with open("config") as f:
     content = f.readlines()
 consumer_key = content[0].rstrip()
@@ -32,6 +32,11 @@ access_token_secret = content[3].rstrip()
 
 conn = boto.sqs.connect_to_region("us-west-2")
 queue_sns = conn.create_queue('cloudcomp')
+queue_sns_sentiment = conn.create_queue('sentimentcloud')
+c = boto.sns.connect_to_region("us-west-2")
+topicname = "cloudcomp"
+topicarn = c.create_topic(topicname)
+
 
 config = {
   'user': 'jessicafan',
@@ -68,8 +73,8 @@ class StdOutListener(tweepy.StreamListener,):
             #print '@%s: %s' % (decoded['user']['screen_name'], decoded['text'].encode('ascii', 'ignore'))
             user = decoded['user']['screen_name']
             text = decoded['text'].encode('ascii', 'ignore')
-            print "________"
-            print text
+           # print "________"
+            #print text
             if decoded["geo"] == None:
                 pass
             else:
@@ -80,29 +85,11 @@ class StdOutListener(tweepy.StreamListener,):
                 lng = geolocation[1]
                 cnx = mysql.connector.connect(**config)
                 cursor = cnx.cursor()
-                print location
+               # print location
                 test = ("INSERT INTO tweet "
                     "(keyword, lat, lng, tweet, tweet_id) "
                     "VALUES (%s, %s, %s, %s, %s)")
-              
-                '''
-                m.message_attributes = {
-                    "user":user,
-                    "geolocation":geolocation,
-                    "location":location,
-                    "lat":lat,
-                    "lng":lng,
-                    "tweet":text,
-                    "tweet_id":tweet_id,
-                    "datetime": time.strftime("%b %d %Y %H:%M:%S", time.gmtime())
-                }'''
-                
-                queue_sns.write(m)
-                rs = queue_sns.get_messages()
-                m = rs[0]
-                print m.get_body()
-
-
+            
                 data_one = (user,lat,lng,text,tweet_id)
                 cursor.execute(test, data_one)
                 cnx.commit()
@@ -124,24 +111,42 @@ def stream_tweet(keyword):
     stream = tweepy.Stream(auth, l)
     stream.filter()
 
+@app.route('/subscribe', methods=['POST','GET'])
+def subscribe():
+    print "_____________________SUBSCRIPTION___________________"
+    headers = request.headers
+    print headers
+    header_type = headers.get('X-Amz-Sns-Message-Type')
+    if header_type == "SubscriptionConfirmation":
+        print "yes!"
+    obj = json.loads(request.data)
 
+
+    return '', 200
 
 @app.route('/')
-def hello_world():  
+def hello_world(): 
+    '''
+    ip="http://160.39.138.189/subscribe"
+    subscription = c.subscribe("arn:aws:sns:us-west-2:708326387433:cloudcomp", "http", ip)
+    print "subscription: "
+    print subscription
+
+    r=rget(ip)
+
+    if r.status_code == 200:
+        print "we're good"
+    else:
+        print r.status_code
+    '''
     author = "Me"
     name = "You"
     return render_template('index.html', author=author, name=name)
-
 
 @app.route('/compute', methods = ['POST'])
 def signup():
     keyword = request.form['keyword']
     print("Finding keyword " + keyword + " ")
-
-    print keyword
-    #__________Using keyword to find a list of streaming tweets w that keyword_____
-    #initialization 
-    print "Showing all new tweets for #programming:"
     return redirect('/showmap/'+keyword)
 
 @app.route('/showmap/<keyword>')
@@ -153,6 +158,8 @@ def showmap(keyword):
     cursor.execute(query)
     print "done:"
 
+    #reads all database results into an array 
+    #writes all database results into AWS SNS
     db = []
     for (lat, lng, tweet) in cursor:
         db.append([tweet,lat, lng])
@@ -161,6 +168,8 @@ def showmap(keyword):
         m.set_body(body) 
         queue_sns.write(m)
 
+        '''
+    #now we process tweets? 
     msg = queue_sns.get_messages()
     if len(msg)>0:
         body = msg[0].get_body()
@@ -175,7 +184,7 @@ def showmap(keyword):
     m = rs[0]
     print m.get_body()
                 
-
+    '''
 
     print len(db)
     cursor.close()
@@ -200,17 +209,54 @@ def start_stream():
     #stream.filter(track="a")
     stream.filter(locations=[-179.9,-89.9,179.9,89.9])
 
+def startworker1():
+    #running forever 
+    while True:
+        msg = queue_sns.get_messages()
+        #print len(msg)
+        #print type(msg)
+        while(len(msg)>0):
+            body_str = msg[0].get_body()
+            queue_sns.delete_message(msg[0])
+            #print body_str
+            body = body_str.split("|")
+            lat = body[0]
+            #print "lat " + lat
+            lng = body[1]
+            #print "lng " + lng
+            tweet = body[2]
+            #print "tweet " + tweet
+            time = body[3]
+            #print "time " + time
+            response = alchemyapi.sentiment("text", tweet)
+            m = Message()
+            if 'docSentiment' not in response:
+                response = "neutral"
+            else:
+                response = response["docSentiment"]["type"]
+
+            added_sentiment = body_str +"|"+ response
+            #print "new Body: " + added_sentiment
+            m.set_body(added_sentiment)
+            queue_sns_sentiment.write(m)
+            msg = queue_sns.get_messages()
+
+
 def runThread():
     st = threading.Thread( target = start_stream ) #start thread at very beginning 
+    worker1 = threading.Thread(target= startworker1)
     st.start()
+    worker1.start()
 
 if __name__ == '__main__':
     cnx = mysql.connector.connect(**config)
     cursor = cnx.cursor()
     print cnx
     #app.run(host='0.0.0.0')
+
     app.before_first_request(runThread)
-    app.run()
+    print threading._active
+    app.run(host='0.0.0.0',port=5005)
 
 
     
