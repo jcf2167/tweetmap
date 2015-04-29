778fc4346f5ad646cf4aa963825c6cc14cf770c5
jessica fan <jcf2167@columbia.edu>
2015-04-29 12:53:18 -0400

    implemented websockets for smooth updates)

--- application.py
+++ application.py
@@ -15,16 +15,23 @@ from alchemyapi import AlchemyAPI
 import boto.sqs
 from boto.sqs.message import Message
 
-#from flaskext.mysql import MySQL
+from gevent import monkey
+monkey.patch_all()
 
-print "working"
+import time
+from threading import Thread
+from flask import Flask, render_template, session, request
+from flask.ext.socketio import SocketIO, emit
 
 DATABASE = 'coord.db'
 app = Flask(__name__)
+app.debug = True
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
+
 alchemyapi = AlchemyAPI()
 
 sentimentdb = []
-fake_db=[]
 
 with open("config") as f:
     content = f.readlines()
@@ -107,10 +114,10 @@ def stream_tweet(keyword):
 
 @app.route('/subscribe', methods=['POST','GET'])
 def subscribe():
-    print "_____________________SUBSCRIPTION___________________"
+    #print "_____________________SUBSCRIPTION___________________"
     headers = request.headers
-    print headers        
-    print "_____________________"
+    #print headers        
+    #print "_____________________"
     obj = json.loads(request.data)
     #print obj
 
@@ -120,60 +127,56 @@ def subscribe():
         print "yes!"
         obj = json.loads(request.data)
         subscribe_url = obj[u'SubscribeURL']
-        print "subscribe_url!!"
-        print subscribe_url
-        print "_____________________ end SUBSCRIPTION___________________"
+        #print "subscribe_url!!"
+        #print subscribe_url
+        #print "_____________________ end SUBSCRIPTION___________________"
         r = rget(subscribe_url)
         
         print "subscription confirmd"
     elif header_type == "Notification":
-        print "NOTIVARTION"
+        #print "NOTIVARTION"
         #print request.body
-        print obj
-        print obj[u'Timestamp']
+        #print obj
+        #print obj[u'Timestamp']
         if 'Subject' in obj:
-            print "message is in obj"
+            #print "message is in obj"
             msg = obj[u'Message']
-            print msg
+            #print msg
             body = obj[u'Subject']
             body = body.split("|")
-            print "this is the body array"
-            print body
+            #print "this is the body array"
+            #print body
             lat = body[0]
-            print lat
+           # print lat
             #print "lat " + lat
             lng = body[1]
-            print lng
+            #print lng
             #print "tweet " + tweet
             time = body[2]
-            print time
+            #print time
             sentiment = body[3]
-            print sentiment
+            #print sentiment
             datapoint = [msg, lat,lng, time, sentiment]
             sentimentdb.append(datapoint)
+            socketio.emit('my response',
+                      {'data': 'Server generated event',
+                        'lat':lat,
+                        'lng': lng,
+                        'sentiment': sentiment
 
-
+                      },
+                      namespace='/test')
     return '', 200
 
+@socketio.on('my event', namespace='/test')
+def test_message(message):
+    session['receive_count'] = session.get('receive_count', 0) + 1
 
 @app.route('/')
 def hello_world(): 
     author = "Me"
     name = "You"
     return render_template('index.html', author=author, name=name)
-
-def trythis():
-    ip="http://"+my_ip+":5005/subscribe"
-    subscription = c.subscribe("arn:aws:sns:us-west-2:708326387433:cloudcomp", "http", ip)
-    print "subscription: "
-    print subscription
-
-    r=rget(ip)
-
-    if r.status_code == 200:
-        print "we're good"
-    else:
-        print r.status_code
 
 @app.route('/compute', methods = ['POST'])
 def signup():
@@ -183,12 +186,11 @@ def signup():
     cnx = mysql.connector.connect(**config)
     cursor = cnx.cursor()
     query = "SELECT lat, lng, tweet FROM tweet WHERE tweet LIKE \"%" + keyword +"%\""
-    print "cursor is excuting: "
+    #print "cursor is excuting: "
     cursor.execute(query)
-    print "done:"
-
-    #reads all database results into an array 
-    #writes all database results into AWS SNS
+    #print "done:"
+   # queue_sns.purge()
+    
     db = []
     for (lat, lng, tweet) in cursor:
         db.append([tweet,lat, lng])
@@ -196,31 +198,27 @@ def signup():
         body = str(lat)+"|"+str(lng)+"|"+str(tweet)+"|"+str(time.strftime("%b %d %Y %H:%M:%S", time.gmtime()))
         m.set_body(body) 
         queue_sns.write(m)
+
+    print "====================================================="
+    print "============about to push into html========================================="
+    print len(db)
+    print "====================================================="
+   
     cursor.close()
     cnx.close()
     return redirect('/showmap/'+keyword)
 
 @app.route('/showmap/<keyword>')
 def showmap(keyword):
-    print "====================================================="
-    print "============about to push into html========================================="
-    print len(sentimentdb)
-    print "====================================================="
 
-   
     return render_template('map.html',keyword=keyword, db=sentimentdb)
-
 
 
 def start_stream():
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret) 
     auth.set_access_token(access_token, access_token_secret)
-    print "dubug -1"
     l = StdOutListener()
-    print "debug -2"
     stream = tweepy.Stream(auth, l)
-    print "debug -3"
-    #stream.filter(track="a")
     stream.filter(locations=[-179.9,-89.9,179.9,89.9])
 
 def startworker1():
@@ -228,12 +226,9 @@ def startworker1():
     while True:
         msg = queue_sns.get_messages()
         msgjson={}
-        #print len(msg)
-        #print type(msg)
         while(len(msg)>0):
             body_str = msg[0].get_body()
             queue_sns.delete_message(msg[0])
-            #print body_str
             body = body_str.split("|")
             lat = body[0]
             msgjson["lat"]=lat
@@ -248,8 +243,10 @@ def startworker1():
             msgjson["time"]=time
             #print "time " + time
             response = alchemyapi.sentiment("text", tweet)
+            print response
             m = Message()
             if 'docSentiment' not in response:
+                print "docSentiment not in reponse"
                 response = "neutral"
             else:
                 response = response["docSentiment"]["type"]
@@ -263,13 +260,11 @@ def startworker1():
             msg = queue_sns.get_messages()
             json_data = json.dumps(msgjson)
             topicarn="arn:aws:sns:us-west-2:708326387433:cloudcomp"
-            print "about to publish"
+            #print "about to publish"
             print str(added_sentiment)
             tempstr = lat+"|"+lng+"|"+time+"|"+response
-            print "Type tempstr"
-            print tempstr
             added_sentiment = added_sentiment.encode("utf8")
-            print type(added_sentiment)
+            #print type(added_sentiment)
             tempstr=tempstr.encode("utf8")
             publication = c.publish(topicarn, message=tweet,  subject=tempstr)
 
@@ -284,13 +279,7 @@ def runThread():
 if __name__ == '__main__':
     cnx = mysql.connector.connect(**config)
     cursor = cnx.cursor()
-    print cnx
-    #app.run(host='0.0.0.0')
-
-
     app.before_first_request(runThread)
-    print threading._active
-    app.run(host='0.0.0.0',port=5005)
+    socketio.run(app, host='0.0.0.0', port=5004)
 
 
-    
--- test_sns.py
+++ test_sns.py
@@ -1,12 +1,15 @@
 import boto.sns
 import urllib2
 from requests import get as rget
+
+
 c = boto.sns.connect_to_region("us-west-2")
 my_ip = urllib2.urlopen('http://ip.42.pl/raw').read()
 
 topicname = "cloudcomp"
 topicarn = c.create_topic(topicname)
-ip="http://"+my_ip+":5005/subscribe"
+print topicarn
+ip="http://"+my_ip+":5004/subscribe"
 subscription = c.subscribe("arn:aws:sns:us-west-2:708326387433:cloudcomp", "http", ip)
 print "subscription: "
 print subscription
@@ -14,6 +17,7 @@ r=rget(ip)
 print r
 if r.status_code == 200:
         print "we're good"
+        r=rget(ip)
 else:
 	print "we're bad"
         print r.status_code
